f51a480c16bb3d1033d45243b657c34fee691c1e
Joachim Fornallaz <fjoachim@gmail.com>
2013-11-10 20:26:21 -0800

    Added umbrella header.

---
+++ CoreSupport.h
@@ -0,0 +1,12 @@
+//
+//  CoreSupport.h
+//  MessageClient
+//
+//  Created by Joachim Fornallaz on 21.10.12.
+//  Copyright (c) 2012 Joachim Fornallaz. All rights reserved.
+//
+
+#import "NSData+CoreSupport.h"
+#import "NSDate+CoreSupport.h"
+#import "NSString+CoreSupport.h"
+#import "NSURL+CoreSupport.h"
